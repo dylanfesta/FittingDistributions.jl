f9139ced2eacdb620a06142e16814f8592b103ec
darksnail <darksnail@gmail.com>
2018-11-08 13:23:51 -0500

    tested with missing data, it works!

--- test/stanfit.jl
+++ test/stanfit.jl
@@ -7,15 +7,20 @@ using Statistics,LinearAlgebra, StatsBase
 const D = FittingDistributions
 
 ##
-n_reps=5
+n_reps=25
 n_models= 8
-n_views=30
+n_views=10
 
 alphas_test = fill(0.1,n_models)
 bins,qvals,mock_data= D.mock_fit_problem(n_reps,n_models, n_views;
-        ground_d = 3)
+        ground_d = 6)
 
-fit_problem = D.SpkToFit(mock_data,bins,qvals,D.DFitDirich(alphas_test))
+mock_data_miss =  let mk = convert(Array{Union{Float64,Missing}},mock_data)
+  mk[3:end,[1,4,6]].=missing
+  mk
+end
+
+fit_problem = D.SpkToFit(mock_data_miss,bins,qvals,D.DFitDirich(alphas_test))
 
 q_data = D.get_Q_ofdata(fit_problem)
 D.set_stan_folder("/home/dfesta/.cmdstan-2.18.0")
@@ -29,12 +34,11 @@ bins = LinRange(0,1.2,20) |> collect
 pp = let
   plot()
   for p=1:8
-    h = fit(Histogram, selectdim(alphas,1,p), bins, closed=:right) |> normalize
+    h = fit(Histogram, selectdim(alphas,1,p), bins, closed=:left) |> normalize
     bar!(h,opacity=0.3)
   end
   plot!()
 end
-plot(pp)
 
 histogram(alphas[2,:],opacity=0.5,leg=false,nbins=10)
 histogram!(alphas[2,:],opacity=0.5,leg=false)
